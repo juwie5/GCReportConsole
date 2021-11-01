a410751ff50292395ad3f21b9b5778cefa597188
Adejuwon Oshadipe <bibire95@gmail.com>
2021-11-01 10:56:10 +0100

    Initial Commit

---
+++ src/components/Login.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container">
+    <div>
+      <div class="head">
+        <img src="../assets/gclogo.svg" alt="">
+        <h3>Winners Golden Chance</h3>
+      </div>
+      <div class="frmgrp">
+        <h3>LOGIN</h3>
+        <form action="" id="loginform">
+          <label for="Username"></label>
+          <input type="email" name="email" placeholder="Username">
+          <label for="Password"></label>
+          <input type="password" name="password" placeholder="Password">
+          <select name="worklist" id="" form="loginform">
+            <option ></option>
+            <option value="home">Home</option>
+            <option value="Office">Office</option>
+          </select>
+          <input type="submit">
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data(){
+    return{
+      emai:null,
+      password: null,
+      feedback: null,
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.container{
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+}
+.head{
+  display: flex;
+}
+
+.frmgrp{
+  display: flex;
+  flex-direction: column;
+
+ form{
+   display:flex;
+   flex-direction: column;
+ }
+}
+</style>
